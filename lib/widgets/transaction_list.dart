3cbb8e1f2515395a4c502101496247221eed6b80
Nilesh Srivastava <nilesh.sri02@gmail.com>
2022-11-24 00:34:46 +0530

    Added transaction list file

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class tracker extends StatefulWidget {
+  const tracker({Key key}) : super(key: key);
+
+  @override
+  State<tracker> createState() => _trackerState();
+}
+
+class _trackerState extends State<tracker> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
