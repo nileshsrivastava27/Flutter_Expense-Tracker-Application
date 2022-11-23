b5d57e429fa875d5371cbb90df4c5c1bec73b20b
Nilesh Srivastava <nilesh.sri02@gmail.com>
2022-11-24 01:43:09 +0530

    added new and user transaction files

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
 
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
 
-class tracker extends StatefulWidget {
-  const tracker({Key key}) : super(key: key);
+  TransactionList(this.transactions);
 
-  @override
-  State<tracker> createState() => _trackerState();
-}
-
-class _trackerState extends State<tracker> {
   @override
   Widget build(BuildContext context) {
-    
+    return Column(
+      children: transactions.map((tx) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  '\$ ${tx.amount}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tx.title,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(tx.date),
+                    style: TextStyle(
+                        color: Colors.grey, fontWeight: FontWeight.normal),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList(),
+    );
   }
 }
